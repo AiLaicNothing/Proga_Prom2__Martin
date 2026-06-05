07fe1198b23f4e77e3b3e7ba8e6ce3f63ec6a8c4
AiLaicNothing <ailaicnothing@gmail.com>
2026-06-04 21:35:56 -0500

    Update PlayerController.cs

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -68,7 +68,7 @@ public class PlayerController : MonoBehaviour
 
         Vector3 velocity = moveDir * desiredSpeed;
 
-        rb.linearVelocity = new Vector3(moveDir.x * moveSpeed, rb.linearVelocity.y, moveDir.z * moveSpeed);
+        rb.linearVelocity = new Vector3(velocity.x, rb.linearVelocity.y, velocity.z);
     }
 
     private void HandleRot()
